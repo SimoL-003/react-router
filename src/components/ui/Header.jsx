84064ffed76a0bd1d833ffe2f57ae31152aef144
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-11-22 19:41:50 +0100

    style(products): improve UI

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,7 @@
 import Home from "../../pages/Home";
 import AboutUs from "../../pages/AboutUs";
 import Products from "../../pages/Products";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   const nav = [
@@ -20,12 +20,23 @@ export default function Header() {
   ];
 
   return (
-    <header className="min-h-20 bg-blue-300 flex items-center justify-center">
-      <nav>
-        <ol className="flex gap-16 items-center">
+    <header className="h-20 bg-white flex items-center justify-center">
+      <nav className="flex justify-between items-center w-[95%]">
+        <Link to="/" className="text-3xl font-bold text-slate-900">
+          Shop Online
+        </Link>
+        <ol className="flex gap-10 items-center">
           {nav.map((cur, index) => (
-            <li key={index}>
-              <NavLink to={cur.path}>{cur.page}</NavLink>
+            <li
+              key={index}
+              className="text-slate-700 font-medium text-base lg:text-lg"
+            >
+              <NavLink
+                to={cur.path}
+                className="hover:text-slate-900 hover:border-b transition-all duration-200"
+              >
+                {cur.page}
+              </NavLink>
             </li>
           ))}
         </ol>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -10,17 +10,33 @@ export default function Products() {
   }, []);
 
   return (
-    <section>
+    <section className="bg-slate-100">
       <div className="container">
-        <h1>Products</h1>
+        <hgroup className="pt-10 mb-8">
+          <h1>Our products</h1>
+          <h2>Choose our products</h2>
+        </hgroup>
         <div className="procucts-container">
-          <ul className="grid grid-cols-4">
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {productsList.map((product) => (
-              <li key={product.id} className="product-card">
-                <div className="card-image">
-                  <img src={product.image} alt={product.title} />
+              // PRODUCT CARD
+              <li
+                key={product.id}
+                className="px-4 py-6 rounded-lg bg-white shadow-lg hover:scale-[1.02] hover:shadow-2xl transition-all duration-300"
+              >
+                {/* Card img */}
+                <div className="flex justify-center">
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="h-[200px] aspect-square object-contain"
+                  />
                 </div>
-                <div className="card-text">{product.title}</div>
+
+                {/* Card text */}
+                <div className="mt-4">
+                  <h3 className="py-1">{product.title}</h3>
+                </div>
               </li>
             ))}
           </ul>
